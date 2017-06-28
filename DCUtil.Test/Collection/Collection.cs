2e1ffb84984dec3259cb10f8222abb5c921eec34
Darren Clark <darren.clark@avianis.com>
2017-06-27 21:54:57 -0700

    test for promise

--- DCUtil.Test/Collection/Collection.cs
+++ DCUtil.Test/Collection/Collection.cs
@@ -1,136 +1,136 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using FluentAssertions;
-using System.Collections.Generic;
-using System;
-using System.Linq;
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using FluentAssertions;
+//using System.Collections.Generic;
+//using System;
+//using System.Linq;
 
-namespace DCUtil.Test.Collection
-{
-    [TestClass]
-    public class Collection
-    {
-        [TestClass]
-        public class TryGetPosition
-        {
-            [TestMethod]
-            public void TestSinglePredicateFound()
-            {
-                var items = new[] { 1, 2, 3 };
+//namespace DCUtil.Test.Collection
+//{
+//    [TestClass]
+//    public class Collection
+//    {
+//        [TestClass]
+//        public class TryGetPosition
+//        {
+//            [TestMethod]
+//            public void TestSinglePredicateFound()
+//            {
+//                var items = new[] { 1, 2, 3 };
 
-                var found = items.TryGetPosition(i => i == 3, out var position);
-                found.Should().BeTrue();
-                position.Item1.Should().Be(2);
-            }
+//                var found = items.TryGetPosition(i => i == 3, out var position);
+//                found.Should().BeTrue();
+//                position.Item1.Should().Be(2);
+//            }
 
-            [TestMethod]
-            public void TestSinglePredicateNotFound()
-            {
-                var items = new[] { 1, 2, 3 };
+//            [TestMethod]
+//            public void TestSinglePredicateNotFound()
+//            {
+//                var items = new[] { 1, 2, 3 };
 
-                var found = items.TryGetPosition(i => false, out var position);
-                found.Should().BeFalse();
-            }
+//                var found = items.TryGetPosition(i => false, out var position);
+//                found.Should().BeFalse();
+//            }
 
-            [TestMethod]
-            public void TestSingleItemFound()
-            {
-                var items = new[] { 1, 2, 3 };
+//            [TestMethod]
+//            public void TestSingleItemFound()
+//            {
+//                var items = new[] { 1, 2, 3 };
 
-                var found = items.TryGetPosition(3, out var position);
-                found.Should().BeTrue();
-                position.Item1.Should().Be(2);
-            }
+//                var found = items.TryGetPosition(3, out var position);
+//                found.Should().BeTrue();
+//                position.Item1.Should().Be(2);
+//            }
 
-            [TestMethod]
-            public void TestSingleItemNotFound()
-            {
-                var items = new[] { 1, 2, 3 };
+//            [TestMethod]
+//            public void TestSingleItemNotFound()
+//            {
+//                var items = new[] { 1, 2, 3 };
 
-                var found = items.TryGetPosition(4, out var position);
-                found.Should().BeFalse();
-            }
+//                var found = items.TryGetPosition(4, out var position);
+//                found.Should().BeFalse();
+//            }
 
-            private class AlwaysComparer : IEqualityComparer<int>
-            {
-                private bool result;
+//            private class AlwaysComparer : IEqualityComparer<int>
+//            {
+//                private bool result;
 
-                public AlwaysComparer(bool result)
-                {
-                    this.result = result;
-                }
+//                public AlwaysComparer(bool result)
+//                {
+//                    this.result = result;
+//                }
 
-                bool IEqualityComparer<int>.Equals(int x, int y)
-                {
-                    return this.result;
-                }
+//                bool IEqualityComparer<int>.Equals(int x, int y)
+//                {
+//                    return this.result;
+//                }
 
-                int IEqualityComparer<int>.GetHashCode(int obj)
-                {
-                    return 0;
-                }
-            }
+//                int IEqualityComparer<int>.GetHashCode(int obj)
+//                {
+//                    return 0;
+//                }
+//            }
 
-            [TestMethod]
-            public void TestSingleItemWithComparerFound()
-            {
-                var comparer = new AlwaysComparer(true);
-                var items = new[] { 1, 2, 3 };
+//            [TestMethod]
+//            public void TestSingleItemWithComparerFound()
+//            {
+//                var comparer = new AlwaysComparer(true);
+//                var items = new[] { 1, 2, 3 };
 
-                var found = items.TryGetPosition(3, comparer, out var position);
-                found.Should().BeTrue();
-                position.Item1.Should().Be(0); // matches 1
-            }
+//                var found = items.TryGetPosition(3, comparer, out var position);
+//                found.Should().BeTrue();
+//                position.Item1.Should().Be(0); // matches 1
+//            }
 
-            [TestMethod]
-            public void TestSingleItemWithComparerNotFound()
-            {
-                var comparer = new AlwaysComparer(false);
-                var items = new[] { 1, 2, 3 };
+//            [TestMethod]
+//            public void TestSingleItemWithComparerNotFound()
+//            {
+//                var comparer = new AlwaysComparer(false);
+//                var items = new[] { 1, 2, 3 };
 
-                var found = items.TryGetPosition(3, comparer, out var position);
-                found.Should().BeFalse(); // never equal
-            }
+//                var found = items.TryGetPosition(3, comparer, out var position);
+//                found.Should().BeFalse(); // never equal
+//            }
 
-            [TestMethod]
-            public void TestNestedPredicateFound()
-            {
-                var items = new[] {
-                     new int[] { 1, 2, 3 },
-                     new int[] { 4, 5, 6 }
-                };
+//            [TestMethod]
+//            public void TestNestedPredicateFound()
+//            {
+//                var items = new[] {
+//                     new int[] { 1, 2, 3 },
+//                     new int[] { 4, 5, 6 }
+//                };
 
-                var found = items.TryGetPosition(i => i == 3, out var position);
-                found.Should().BeTrue();
-                position.Item1.Should().Be(0);
-                position.Item2.Should().Be(2);
-            }
+//                var found = items.TryGetPosition(i => i == 3, out var position);
+//                found.Should().BeTrue();
+//                position.Item1.Should().Be(0);
+//                position.Item2.Should().Be(2);
+//            }
 
-            [TestMethod]
-            public void TestNestedPredicateFoundSecondList()
-            {
-                var items = new[] {
-                     new int[] { 1, 2, 3 },
-                     new int[] { 4, 5, 6 }
-                };
+//            [TestMethod]
+//            public void TestNestedPredicateFoundSecondList()
+//            {
+//                var items = new[] {
+//                     new int[] { 1, 2, 3 },
+//                     new int[] { 4, 5, 6 }
+//                };
 
-                var found = items.TryGetPosition(i => i== 4, out var position);
-                found.Should().BeTrue();
-                position.Item1.Should().Be(1);
-                position.Item2.Should().Be(0);
-            }
+//                var found = items.TryGetPosition(i => i== 4, out var position);
+//                found.Should().BeTrue();
+//                position.Item1.Should().Be(1);
+//                position.Item2.Should().Be(0);
+//            }
 
-            [TestMethod]
-            public void TestNestedPredicateNotFoundSecondList()
-            {
-                var items = new[] {
-                     new int[] { 1, 2, 3 },
-                     new int[] { 4, 5, 6 }
-                };
+//            [TestMethod]
+//            public void TestNestedPredicateNotFoundSecondList()
+//            {
+//                var items = new[] {
+//                     new int[] { 1, 2, 3 },
+//                     new int[] { 4, 5, 6 }
+//                };
 
-                var found = items.TryGetPosition(i => i == 7, out var position);
-                found.Should().BeFalse();
-            }
+//                var found = items.TryGetPosition(i => i == 7, out var position);
+//                found.Should().BeFalse();
+//            }
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ DCUtil.Test/Task/Promise/PromiseTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace DCUtil.Test.Task.Promise
+{
+    using DCUtil.Task;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Threading;
+
+    [TestClass]
+    public class PromiseTests
+    {
+        private async Task RecursiveAwait(Task task, int recursionCount)
+        {
+            Debug.WriteLine($"Recursive: {recursionCount}");
+            if (recursionCount-- == 0) return;
+            await RecursiveAwait(task, recursionCount);
+            
+        }
+        
+        [TestMethod]
+        public void RecursiveAwaitBaseLine()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var  task = RecursiveAwait(Task.FromResult(0), 10000);
+            task.Wait();
+            sw.Stop();
+        }
+
+        [TestMethod]
+        public void RecursiveThenBaseLine()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var task =  RecursiveThen(Task.FromResult(0), 10000);
+            task.Wait();
+            sw.Stop();
+        }
+
+        private static Task Empty = Task.FromResult(0);
+
+        private Task RecursiveThen(Task task, int recursionCount)
+        {
+            while(recursionCount-- > 0)
+            {
+                var temp = recursionCount;
+                task = task.Then(() =>
+                {
+                    Debug.WriteLine($"Then: {temp}");
+                }, CancellationToken.None);
+            }
+
+            return task;
+        }
+    }
+}
